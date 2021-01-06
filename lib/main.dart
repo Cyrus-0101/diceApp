9614ca4e20f01a971c9b6c6267b50178cf72d4a8
Toxic254 <cyrus_g@outlook.com>
2021-01-06 20:39:46 +0300

    Completed the app. Now lets see who takes out the trash.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -27,9 +29,47 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  // We create the variable name here so as to
+  // update the variable number when it changes not when the app. re-renders
+  int leftDiceNumber = Random().nextInt(6) + 1;
+  int rightDiceNumber = Random().nextInt(6) + 1;
+
+  void randomNumber() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: FlatButton(
+            onPressed: () {
+              setState(() {
+                randomNumber();
+              });
+            },
+            child: Image.asset("assets/images/dice$leftDiceNumber.png"),
+          )),
+          Expanded(
+              child: FlatButton(
+            onPressed: () {
+              setState(() {
+                randomNumber();
+              });
+            },
+            child: Image.asset("assets/images/dice$rightDiceNumber.png"),
+          )),
+        ],
+      ),
+    );
   }
 }
